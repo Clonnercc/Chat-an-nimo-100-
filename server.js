74ac9ea25319e24e9ab4913061ee5202d0bd81ff
Clonnercc <151041849+Clonnercc@users.noreply.github.com>
2025-12-10 02:07:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,12 +9,28 @@ const io = new Server(server);
 app.use(express.static("public"));
 
 io.on("connection", (socket) => {
-  socket.on("msg", (texto) => {
-    io.emit("msg", {
+
+  socket.on("joinRoom", (room) => {
+    socket.join(room);
+    const count = io.sockets.adapter.rooms.get(room)?.size || 0;
+    io.to(room).emit("online", count);
+  });
+
+  socket.on("msg", ({ room, texto }) => {
+    io.to(room).emit("msg", {
       id: socket.id.slice(0, 5),
-      texto
+      texto,
+      tempo: Date.now()
     });
   });
+
+  socket.on("disconnecting", () => {
+    socket.rooms.forEach(room => {
+      const count = io.sockets.adapter.rooms.get(room)?.size || 0;
+      io.to(room).emit("online", count - 1);
+    });
+  });
+
 });
 
 const PORT = process.env.PORT || 3000;
